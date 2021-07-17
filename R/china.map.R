1e05c50a5098073c38c6527c17d7df0d957b5b7d
ZhonghuiGai <zhgai@aliyun.com>
2021-07-17 09:28:31 +0800

    version 0.0.1

---
+++ R/china.map.R
@@ -0,0 +1,78 @@
+#' China map based ggplot2 version 1
+#'
+#' @param data a data frame containing values to fill the provinces
+#' @param fill.variable a numeric vector
+#' @param limits the range of fill variable
+#' @param midpoint the midpoint of fill variable
+#' @param breaks the breaks of fill variable
+#'
+#' @author ZhonghuiGai
+#' @return
+#' @export
+#'
+#' @examples
+#' data <- openxlsx::read.xlsx("provincedata.xlsx", sheet = 1, rowNames = F, colNames = T)
+#' library(showtext)
+#' showtext_auto(enable = TRUE)
+#' font_add('Songti', 'Songti.ttc','STKaiti','STXihei')
+#' china.map(data = data, fill.variable = "value",
+#' limits = c(4000, 19000),
+#' midpoint = 12000,
+#' breaks = c(5000, 8000, 11000, 14000, 17000))
+china.map <- function(data, fill.variable = "value",
+                      limits = c(4000, 19000), midpoint = 12000,
+                      breaks = c(5000, 8000, 11000, 14000, 17000)){
+  map = "china"
+  library(sf)
+  nine.lines <- readr::read_rds("nine.lines.rds")
+  if (map == "china") {
+    china <- readr::read_rds("china.map.rds")
+    center <- openxlsx::read.xlsx(xlsxFile = "AdmiCenter.xlsx",
+                                  sheet = 1, colNames = TRUE, rowNames = FALSE)
+    p <- dplyr::full_join(china, data, type = "full") |>
+    map.theme() +
+      geom_sf(mapping = aes(fill = get(fill.variable)), color = "white",
+              size = 0.02, show.legend = TRUE, na.rm = T)  +
+      geom_sf(data = nine.lines, color = "black", size = 0.5) +
+      geom_text(data = center, aes(x = jd, y = wd, label = ShortName),
+                fontface = "bold", colour = "black", size = 3) +
+      scale_fill_gradient2(name = fill.variable, limits = limits,
+                           low = "#5dcca0", mid = "#f8d248", high = "#ff445d",
+                           midpoint = midpoint, breaks = breaks)
+  }else if (map == "guojie") {
+    guojie <- readr::read_rds("guojie.rds")
+    p <- map.theme(data = guojie) + geom_sf(show.legend = FALSE)
+  }else if (map == "nine.lines") {
+    p <- map.theme(data = nine.lines) + geom_sf(show.legend = FALSE)
+  }
+  return(p)
+}
+
+#' @param data a data frame
+#'
+#' @return a ggplot object
+map.theme <- function(data){
+  p <- ggplot(data = data) +
+    theme(panel.background = element_blank(),
+                   panel.grid = element_blank(),
+                   axis.text = element_blank(),
+                   axis.title = element_blank(),
+                   axis.ticks = element_blank(),
+                   legend.background = element_blank(),
+                   legend.title = element_blank(),
+                   legend.text = element_text(size = 4, face = "bold"),
+                   legend.key.size=unit(0.3, "cm"),
+                   legend.key.width=unit(0.1, "cm"),
+                   legend.position = c(0.2, 0.3),
+                   legend.spacing.x = unit(0.001, 'lines')) +
+    xlab(NULL) + ylab(NULL) +
+    guides(fill = guide_colorbar(title = "HP",
+                                 label.position = "right",
+                                 title.position = "top", title.vjust = 1,
+                                 frame.colour = "white",
+                                 barwidth = 0.3,
+                                 barheight = 3)) +
+    scale_x_continuous(expand = c(0,0)) +
+    scale_y_continuous(expand = c(0,0))
+  return(p)
+}
